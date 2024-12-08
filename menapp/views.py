b12dd58ade553115efbaed5a56c37d02d595fdf7
koles <kolesnikov.pavel.sergeevich@gmail.com>
2024-12-08 13:51:41 +0700

    прописали возможность отправки post запросов с записью в БД

--- menapp/views.py
+++ menapp/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 
 from rest_framework import generics
@@ -7,12 +8,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
+
 class MenAPIView(APIView):
     def get(self, request):  # Метод отвечающий за обработку get запросов
-        return Response({'title': 'Майкл Джексон'})  # Response вернет просто фиксированные данные в виде JSON строки
+        lst = Men.objects.all().values()  # Получаем список всех записей из БД Men c помощью values
+        return Response({'posts': list(lst)})  # Возвращаем список всех записей с ключем posts
 
     def post(self, request):  # Метод отвечающий за обработку post запросов
-        return Response({'title': 'Майк Тайсон'})  # Вернем предопределенный JSON при отправке post запроса
+        post_new = Men.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id'],
+        )
+        return Response({'post': model_to_dict(post_new)})  # Вернем наши добавленные данные, преобразовав из в словарь
 
 
 # class MenAPIView(generics.ListAPIView):
